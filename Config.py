583a05f15752e3555a0cece3184f6137244b4610
Ilan <ilan.vasilevsky@gmail.com>
2022-12-31 16:00:34 +0200

    Clustering Progress

--- Config.py
+++ Config.py
@@ -7,7 +7,7 @@
 # Paths to the data files
 training_data_path = 'data/ctr_dataset_train.csv'
 test_data_path = 'data/ctr_dataset_test.csv'
-clustering_data_path = 'data/ctr_dataset_train.csv'
+clustering_data_path = 'data/clustering_data.csv'
 
 binary_columns = ['clicked',
                     'app_cat_art_and_design',
